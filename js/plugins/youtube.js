1269be52e385a4c5d3b89f6cb10d3152ed1adf82
Braden Hutchinson <braden.hutchinson@hotmail.com>
2019-01-09 10:41:20 -0700

    removed deprecated youtube player stuff

--- js/plugins/youtube.js
+++ js/plugins/youtube.js
@@ -70,17 +70,16 @@
 							autoplay: 1,
 							cc_load_policy: 0,
 							controls: 0,
-							disablekb: 1,
+							disablekb: 0,
 							enablejsapi: 1,
-							start: startTime,
 							end: endTime === -1 ? void 0 : endTime,
+							fs: 0,
 							iv_load_policy: 3,
 							modestbranding: 1,
 							origin: location.origin,
 							playsinline: 1,
-							rel: 0,
-							showinfo: 0,
-							fs: 0
+							start: startTime,
+							rel: 0
 						},
 						events: {
 							onReady: function(event){
